7e352749618d567247f71d9a45938f171faa1fb2
Manda Yugana <manda.yugana@gmail.com>
2020-08-17 12:41:34 +0700

    Add FileLogger.

---
+++ src/Manday/Log/Logger/FileLogger.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Manday\Log\Logger;
+
+use Manday\Log\Logger\AbstractLogger;
+use RuntimeException;
+
+class FileLogger extends AbstractLogger
+{
+    protected $filename;
+
+    public function __construct(string $filename)
+    {
+        // check whether file is writable
+        // if file not exists, create it
+        $handle = @fopen($filename, 'c');
+        if ($handle === false) {
+            throw new RuntimeException("'$filename' is not writable file");
+            
+        }
+        fclose($handle);
+        $this->filename = $filename;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function log(string $tag, string $message, array $context = [], $level): void
+    {
+        $out = sprintf(
+            "%s [%s] [%s] %s\n",
+            date('c'),
+            $tag,
+            $level,
+            $this->interpolate($message, $context)
+        );
+        @file_put_contents($this->filename, $out, FILE_APPEND | LOCK_EX);
+    }
+}
